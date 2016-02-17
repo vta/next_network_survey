67ec2451363795be0bed7e731dbc89c080273187
Falconer <falconer_k@vtaisg708384.local>
2016-02-17 10:09:50 -0800

    Added basic implementation for choosnig a "range" or "radio" type question.

--- questions.js
+++ questions.js
@@ -1,14 +1,22 @@
 [
  {
   "question":"what is a?",
-  "answers":["a","b","c","d"]
+  "answers":["a","b","c","d"],
+  "type":"radio"
  },
  {
   "question":"what is b?",
-  "answers":["a","b","c","d","e"]
+  "answers":["a","b","c","d","e"],
+  "type":"radio"
  },
  {
   "question":"what is c?",
-  "answers":["a","b","c","d"]
+  "answers":["a","b","c","d"],
+  "type":"radio"
+ },
+  {
+  "question":"A or B?",
+  "answers":["A","B"],
+  "type":"range"
  }
 ]
---
+++ readme.md
@@ -0,0 +1,4 @@
+
+
+TODO:
+prevent form submission twice
