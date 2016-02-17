cf0380bc66ea300ec05ad1eef8dd5d0b97a4449c
Falconer <falconer_k@vtaisg708384.local>
2016-02-17 14:09:27 -0800

    added test questions, cleaned up code a bit.

--- questions.js
+++ questions.js
@@ -18,5 +18,20 @@
   "question":"A or B?",
   "answers":["A","B"],
   "type":"range"
+ },
+   {
+  "question":"A and/or B?",
+  "answers":["A","B"],
+  "type":"checkbox"
+ },
+  {
+  "question":"Malformed radio question test",
+  "answers":["A"],
+  "type":"radio"
+ }, 
+  {
+  "question":"Malformed range question test",
+  "answers":["A"],
+  "type":"range"
  }
 ]
