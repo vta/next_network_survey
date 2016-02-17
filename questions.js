3c4870f6ecff04ad41793d39c87c14b96e2a43db
Falconer <falconer_k@vtaisg708384.local>
2016-02-16 16:16:39 -0800

    Basic functional form implementation based on JSON input and radio buttons

---
+++ questions.js
@@ -0,0 +1,14 @@
+[
+ {
+  "question":"what is a?",
+  "answers":["a","b","c","d"]
+ },
+ {
+  "question":"what is b?",
+  "answers":["a","b","c","d","e"]
+ },
+ {
+  "question":"what is c?",
+  "answers":["a","b","c","d"]
+ }
+]
